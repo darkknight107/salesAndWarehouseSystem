7ba2f3098e2757107a9811b5239ce108979a2a82
darkknight107 <001shirish@gmail.com>
2018-05-22 22:38:59 +1000

    Adding tests on searchProduct Test
    -added setter and getter tests

--- Implementation/ABCInventory/src/test/java/com/junittest/SearchProductTest.java
+++ Implementation/ABCInventory/src/test/java/com/junittest/SearchProductTest.java
@@ -11,7 +11,8 @@ import static junit.framework.TestCase.assertEquals;
 public class SearchProductTest {
     SearchProduct searchProduct= null;
     Field productItemField= null;
-
+    Field productSizeField= null;
+    Field productCodeField= null;
 
     @Before
     public void setUp() throws Exception {
@@ -21,24 +22,61 @@ public class SearchProductTest {
     public void testSetProductItemCode() throws NoSuchFieldException, IllegalAccessException {
         //when
         searchProduct.setProductItemCode("A1");
-
         //then
         productItemField= searchProduct.getClass().getDeclaredField("productItemCode");
         productItemField.setAccessible(true);
         assertEquals("Testing setProductItemCode", productItemField.get(searchProduct), "A1");
     }
-
     @Test
     public void testGetProductItemCode() throws NoSuchFieldException, IllegalAccessException {
         //given
         productItemField = searchProduct.getClass().getDeclaredField("productItemCode");
         productItemField.setAccessible(true);
-        productItemField.set(searchProduct, "A1");
-
+        productItemField.set(searchProduct, "A1100");
         //when
         final String result= searchProduct.getProductItemCode();
-
         //then
-        assertEquals("testing getProductItemCode", result, "A1");
+        assertEquals("testing getProductItemCode", result, "A1100");
     }
+    @Test
+    public void testSetProductSize() throws NoSuchFieldException, IllegalAccessException {
+        //when
+        searchProduct.setProductSize("M");
+        //then
+        productSizeField= searchProduct.getClass().getDeclaredField("productSize");
+        productSizeField.setAccessible(true);
+        assertEquals("Testing setProductSize", productSizeField.get(searchProduct), "M");
+    }
+    @Test
+    public void testGetProductSize() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        productSizeField= searchProduct.getClass().getDeclaredField("productSize");
+        productSizeField.setAccessible(true);
+        productSizeField.set(searchProduct, "XS");
+        //when
+        final String result= searchProduct.getProductSize();
+        //then
+        assertEquals("testing getProductSize", result, "XS");
+    }
+    @Test
+    public void testSetProductCode() throws NoSuchFieldException, IllegalAccessException {
+        //when
+        searchProduct.setProductCode("C1");
+        //then
+        productCodeField= searchProduct.getClass().getDeclaredField("productCode");
+        productCodeField.setAccessible(true);
+        assertEquals("Testing setProductCode", productCodeField.get(searchProduct), "C1");
+    }
+    @Test
+    public void testGetProductCode() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        productCodeField= searchProduct.getClass().getDeclaredField("productCode");
+        productCodeField.setAccessible(true);
+        productCodeField.set(searchProduct,"C2");
+        //when
+        final String result= searchProduct.getProductCode();
+        //then
+        assertEquals("Testing getProductCode", result, "C2");
+    }
+
 }
