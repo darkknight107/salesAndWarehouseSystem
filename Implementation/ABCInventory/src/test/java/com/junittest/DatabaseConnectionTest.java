bd1b4cf517b7b611ea87e4dee6c4b9fbe90c5b1c
darkknight107 <001shirish@gmail.com>
2018-05-24 13:23:25 +1000

    update

---
+++ Implementation/ABCInventory/src/test/java/com/junittest/DatabaseConnectionTest.java
@@ -0,0 +1,17 @@
+package com.junittest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DatabaseConnectionTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void connect() {
+    }
+}
